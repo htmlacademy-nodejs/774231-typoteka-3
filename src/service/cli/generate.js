14002e1c0428a3cb2e89e0b6cc8d90cafb40955c
Karadzikov Aleksandr <karadzikovaleksandr@gmail.com>
2020-12-18 13:49:53 +0300

    Модуль 2 задание 2. Личный проект

--- src/service/cli/generate.js
+++ src/service/cli/generate.js
@@ -2,27 +2,33 @@
 
 const fs = require(`fs`).promises;
 const chalk = require(`chalk`);
-const {CATEGORIES,
-  SENTENCES,
-  TITLES,
+const path = require(`path`);
+const {
   DEFAULT_COUNT,
   FILE_NAME,
   ExitCode} = require(`../constants`);
 const {
   getRandomInt,
+  getData,
   generateDateCreated,
   shuffle
 } = require(`../utils`);
 
-const generateOffers = (count) => (
-  Array(count).fill({}).map(() => ({
-    category: shuffle(CATEGORIES).slice(0, 3),
-    announce: shuffle(SENTENCES).slice(1, 5).join(` `),
-    title: TITLES[getRandomInt(0, TITLES.length - 1)],
-    fullText: shuffle(SENTENCES).slice(1, 7).join(` `),
-    createdDate: generateDateCreated(),
-  }))
-);
+const generateOffers = async (count) => {
+  const TITLES = await getData(path.join(`data`, `titles.txt`));
+  const SENTENCES = await getData(path.join(`data`, `sentences.txt`));
+  const CATEGORIES = await getData(path.join(`data`, `categories.txt`));
+
+  return (
+    Array(count).fill({}).map(() => ({
+      category: shuffle(CATEGORIES).slice(0, 3),
+      announce: shuffle(SENTENCES).slice(1, 5).join(` `),
+      title: TITLES[getRandomInt(0, TITLES.length - 1)],
+      fullText: shuffle(SENTENCES).slice(1, 7).join(` `),
+      createdDate: generateDateCreated(),
+    }))
+  );
+};
 
 module.exports = {
   name: `--generate`,
@@ -35,9 +41,8 @@ module.exports = {
       process.exit(ExitCode.EXIT);
     }
 
-    const content = JSON.stringify(generateOffers(countOffer));
-
     try {
+      const content = JSON.stringify(await generateOffers(countOffer));
       await fs.writeFile(FILE_NAME, content);
       console.info(chalk.green(`Operation success. File created.`));
       process.exit(ExitCode.SUCCESS);
