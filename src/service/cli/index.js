15328ae3dbeb5e119a68cd646fb0b4bf5449acb8
Karadzikov Aleksandr <karadzikovaleksandr@gmail.com>
2020-12-22 15:29:07 +0300

    Модуль 2, задание 3. Личный проект

--- src/service/cli/index.js
+++ src/service/cli/index.js
@@ -3,11 +3,13 @@
 const generate = require(`./generate`);
 const help = require(`./help`);
 const version = require(`./version`);
+const server = require(`./server`);
 
 const Cli = {
   [generate.name]: generate,
   [version.name]: version,
   [help.name]: help,
+  [server.name]: server,
 };
 
 module.exports = {
--- src/service/constants.js
+++ src/service/constants.js
@@ -5,10 +5,19 @@ const USER_ARGV_INDEX = 2;
 const DEFAULT_COUNT = 1;
 const FILE_NAME = `mocks.json`;
 const DEFAULT_COMMAND = `--help`;
+const DEFAULT_PORT = 3000;
 
 const ExitCode = {
   SUCCESS: 0,
   EXIT: 1,
+};
+
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
 };
 
 const TITLES = [
@@ -65,6 +74,8 @@ const CATEGORIES = [
 module.exports = {
   USER_ARGV_INDEX,
   DEFAULT_COUNT,
+  HttpCode,
+  DEFAULT_PORT,
   FILE_NAME,
   TITLES,
   DEFAULT_COMMAND,
